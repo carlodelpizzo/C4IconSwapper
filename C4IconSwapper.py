916239edc72a9d90780d3295eef3365968428668
carlodelpizzo <carlodelpizzo@gmail.com>
2021-08-21 17:58:47 -0400

    added option to overwrite original file

--- C4IconSwapper.py
+++ C4IconSwapper.py
@@ -10,7 +10,6 @@ import base64
 import time
 import random
 
-# exe made with pyinstaller --onefile -w
 version = '3.1b'
 
 # Root Window
@@ -21,23 +20,10 @@ root.resizable(False, False)
 entry_font = 'Helvetica'
 
 # Creating temporary directory
-cur_dir = os.getcwd()
-temp_dir = os.getcwd() + '/temp/'
+cur_dir = os.getcwd() + '/'
+temp_dir = cur_dir + 'temp/'
 if not os.path.isdir(temp_dir):
     os.mkdir(temp_dir)
-
-# Base64 encoded icon
-# Decoding and creating a temporary image file
-win_icon = \
-    """
-AAABAAEAICAAAAEAIACoEAAAFgAAACgAAAAgAAAAQAAAAAEAIAAAAAAAABAAABMLAAATCwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEAAQEBAIODg4RFRUVHBUVFRwVFRUcFRUVHBUVFRwVFRUcDg4OEQMDAwIEBAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEOAgICfQEBAcwBAQHcAQEB3AEBAdwBAQHcAQEB3AEBAdwBAQHMAgICfAEBAQ4AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVFRUA////AAEBAXsAAAD7EBAQ/yAgIP8gICD/ICAg/yAgIP8gICD/ICAg/xAQEP8AAAD7AgICe////wAWFhYAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYGBgAGBgYPAAAAyQ8PD/+srKz/5OTk/+Pj4//j4+P/4+Pj/+Pj4//k5OT/rKys/w8PD/8AAADJBgYGDwYGBgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA8PDwATExMCCQkJDQcHB0QAAADoKioq/+np6f//////8PDw//b29v/29vb/8PDw///////o6Oj/Kioq/wAAAOgHBwdECQkJDgwMDAMPDw8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQEBDgMDA30BAQHKAAAA6QAAAP9tbW3//v7+/+/v7/9PT0//XV1d/11dXf9PT0//7+/v//7+/v9tbW3/AAAA/wAAAOkBAQHMAgICiAUFBRYDAwMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAgIAN/f3wACAgJ9AAAA/A8PD/8oKCj/bGxs/+Xl5f//////7u7u/zU1Nf8AAAD/AAAA/zc3N//v7+///////+Xl5f9sbGz/KCgo/xEREf8AAAD/AgIChzQ0NAENDQ0AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAASEhIAEhISEQEBAcsQEBD/rKys/+vr6//8/Pz/////////////////jY2N/wICAv8CAgL/jY2N//////////////////v7+//r6+v/sbGx/xEREf8BAQHNExMTEhQUFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABUVFQAUFBQeAQEB3iEhIf/i4uL//v7+/+rq6v/p6en//Pz8///////u7u7/ZGRk/2VlZf/u7u7///////z8/P/p6en/6urq//7+/v/i4uL/ISEh/wEBAd4UFBQeFRUVAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAWFhYAAAAAAAEBAVIAAAD2PT09/9zc3P92dnb/Kioq/yoqKv91dXX/6+vr///////29vb/9vb2///////q6ur/dXV1/yoqKv8rKyv/d3d3/9zc3P88PDz/AAAA9gMDA08BAQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQEBAAGBgYGAQEBqgEBAf+Pj4//eHh4/wAAAP8AAAD/AAAA/wAAAP91dXX//v7+/////////////v7+/3V1df8AAAD/AAAA/wAAAP8AAAD/eXl5/42Njf8AAAD/AQEBpAwMDAUJCQkAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACsAAADmHx8f/8PDw/8uLi7/AAAA/wAAAP8AAAD/AAAA/ywsLP/p6en////////////p6en/Kysr/wAAAP8AAAD/AAAA/wAAAP8uLi7/wcHB/x0dHf8AAADkBgYGKgUFBQAAAAAAAAAAAAAAAAAAAAAAAAAAABkZGQAAAAAAAgICaAAAAP5ZWVn/5eXl/ywsLP8AAAD/AAAA/wAAAP8AAAD/LCws/+rq6v///////////+np6f8rKyv/AAAA/wAAAP8AAAD/AAAA/y0tLf/l5eX/VlZW/wAAAP4EBARkAAAAAB0dHQAAAAAAAAAAAAAAAAAAAAAADQ0NADAwMAEBAQGeAAAA/5iYmP//////dXV1/wAAAP8AAAD/AAAA/wAAAP93d3f//v7+/////////////f39/3V1df8AAAD/AAAA/wAAAP8AAAD/d3d3//////+Tk5P/AAAA/wICApr///8AFBQUAAAAAAAAAAAAAAAAAAAAAAAEBAQABAQECwAAAMINDQ3/xsbG///////r6+v/dXV1/yoqKv8qKir/dnZ2/+vr6///////////////////////6urq/3V1df8qKir/Kysr/3Z2dv/r6+v//////8LCwv8MDAz/AAAAvwYGBgoGBgYAAAAAAAAAAAAAAAAAAAAAABMTEwASEhIWAAAA1RwcHP/d3d3////////////8/Pz/6enp/+np6f/8/Pz//////////////////////////////////Pz8/+np6f/p6en//Pz8////////////29vb/xsbG/8AAADTERERFRISEgAAAAAAAAAAAAAAAAAAAAAAFRUVABQUFBkBAQHZHh4e/+Dg4P/////////////////////////////////////////////////////////////////////////////////////////////////g4OD/Hh4e/wEBAdYWFhYXFxcXAAAAAAAAAAAAAAAAAAAAAAAICAgABwcHDgAAAMgTExP/0NDQ/////////////////////////////////////////////////////////////////////////////////////////////////9LS0v8UFBT/AAAAxwQEBA0EBAQAAAAAAAAAAAAAAAAAAAAAAA8PDwAWFhYDAQEBpwQEBP+qqqr/////////////////////////////////////////////////////////////////////////////////////////////////ra2t/wUFBf8BAQGoFBQUAw0NDQAAAAAAAAAAAAAAAAAAAAAARkZGAAAAAAACAgJqAAAA/2FhYf/8/Pz///////////////////////////////////////////////////////////////////////////////////////39/f9lZWX/AAAA/wMDA20AAAAANzc3AAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEAAUFBScAAADfFhYW/8PDw///////////////////////////////////////////////////////////////////////////////////////xcXF/xcXF/8AAADfBAQEJwMDAwAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAICAgANjY2AQMDA4IAAAD+QkJC/+Li4v///////////////////////////////////////////////////////////////////////////+Li4v9CQkL/AAAA/gMDA4EzMzMBCAgIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAGBgYABwcHGAEBAbkAAAD/S0tL/9nZ2f/////////////////////////////////////////////////////////////////V1dX/SEhI/wAAAP8BAQG2BgYGFgQEBAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAMDAwACgoKKwMDA8YAAAD/LCws/56env/r6+v///////////////////////////////////////7+/v/o6Oj/lpaW/ycnJ/8AAAD/AAAAvAAAACQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFdXVwAAAAAABQUFIwEBAaQAAAD3BgYG/zMzM/96enr/srKy/9LS0v/f39//39/f/9HR0f+vr6//dXV1/y4uLv8EBAT/AAAA9QICAp4DAwMfBAQEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABAQEACQkJCwICAlkBAQG+AAAA8wAAAP8GBgb/FBQU/x4eHv8eHh7/ExMT/wUFBf8AAAD/AAAA8AEBAbUCAgJQCwsLCQAAAACysrIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAgIAAQEBAAwMDA4DAwNAAgICfwEBAa0AAADIAQEB2QEBAdkBAQHHAgICrAICAnkDAwM6BAQECwUFBQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACQkJABDQ0MADg4OBQgICA0LCwsZCwsLGQoKCg0WFhYE////AFJSUgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA/////////////////+AH///AA///wAP//4AB//4AAH/8AAA//AAAH/gAAB/4AAAf+AAAH/AAAA/wAAAP8AAAD+AAAA/gAAAH4AAAB+AAAAfgAAAH4AAAB/AAAA/wAAAP8AAAD/gAAB/8AAA//gAAf/8AAP//wAP///gf//////8=
-"""
-icon_data = base64.b64decode(win_icon)
-temp_icon_file = temp_dir + 'icon.ico'
-icon_file = open(temp_icon_file, 'wb')
-icon_file.write(icon_data)
-icon_file.close()
-# root.wm_iconbitmap(temp_icon_file)  # Windows defender flags pyinstaller exe if this is enabled
 
 # Base64 encoded blank image
 # Decoding and creating a temporary image file
@@ -55,13 +41,7 @@ blank = blank_image.resize((128, 128), Image.ANTIALIAS)
 blank = ImageTk.PhotoImage(blank)
 blank_image.close()
 
-# 'Upload Control4 Driver' Frame
-c4z_x = 5
-c4z_y = 5
-
-# 'Upload Replacement Image' Frame
-replacement_x = 0
-replacement_y = 0
+shutil.rmtree(temp_dir)
 
 # Global Variables
 global c4_driver
@@ -187,23 +167,6 @@ class C4zPanel:
         self.file_entry_field.place(x=108 + self.x, y=21 + self.y, anchor='n')
         self.file_entry_field['state'] = DISABLED
 
-    def update_pos(self, new_x=None, new_y=None):
-        if new_x:
-            self.x = new_x
-        if new_y:
-            self.y = new_y
-        # Labels
-        self.blank_image_label.place(x=108 + self.x, y=42 + self.y, anchor='n')
-        self.icon_label.place(x=108 + self.x, y=176 + self.y, anchor='n')
-        self.icon_name_label.place(x=108 + self.x, y=197 + self.y, anchor='n')
-        # Buttons
-        self.open_file_button.place(x=187 + self.x, y=30 + self.y, anchor='w')
-        self.restore_button.place(x=228 + self.x, y=71 + self.y, anchor='n')
-        self.prev_icon_button.place(x=180 + self.x, y=146 + self.y)
-        self.next_icon_button.place(x=230 + self.x, y=146 + self.y)
-        # Entry
-        self.file_entry_field.place(x=108 + self.x, y=21 + self.y, anchor='n')
-
     def update_icon(self):
         icon_image = Image.open(c4_driver.icon_groups[c4_driver.current_icon].path)
 
@@ -265,23 +228,6 @@ class ReplacementPanel:
         self.file_entry_field.place(x=108 + self.x, y=21 + self.y, anchor='n')
         self.file_entry_field['state'] = DISABLED
 
-    def update_pos(self, new_x=None, new_y=None):
-        if new_x:
-            self.x = new_x
-        if new_y:
-            self.y = new_y
-        # Labels
-        self.blank_image_label.place(x=108 + self.x, y=42 + self.y, anchor='n')
-        # Buttons
-        self.open_file_button.place(x=187 + self.x, y=30 + self.y, anchor='w')
-        self.replace_button.place(x=228 + self.x, y=71 + self.y, anchor='n')
-        self.replace_all_button.place(x=228 + self.x, y=71 + self.y, anchor='n')
-        self.replace_button.place(x=228 + self.x, y=71 + self.y, anchor='n')
-        self.prev_icon_button.place(x=180 + self.x, y=146 + self.y)
-        self.next_icon_button.place(x=230 + self.x, y=146 + self.y)
-        # Entry
-        self.file_entry_field.place(x=108 + self.x, y=21 + self.y, anchor='n')
-
     def update_icon(self):
         if replacement_selected:
             icon_image = Image.open(replacement_image_path)
@@ -322,7 +268,6 @@ class ExportPanel:
                                             variable=self.over_orig).place(x=63 + self.x, y=105 + self.y, anchor='w')
 
 
-# Functions
 def upload_c4z():
     global driver_selected
     global replacement_selected
@@ -648,21 +593,21 @@ def export_c4z():
             ran_file_name = 'IcnSwp_'
             for _ in range(0, 6):
                 ran_file_name += str(random.randint(0, 9))
-            if os.path.isfile(cur_dir + '/' + ran_file_name + '.c4z'):
-                os.remove(cur_dir + '/' + ran_file_name + '.c4z')
-            if os.path.isfile(cur_dir + '/' + ran_file_name + '.zip'):
-                os.remove(cur_dir + '/' + ran_file_name + '.zip')
+            if os.path.isfile(cur_dir + ran_file_name + '.c4z'):
+                os.remove(cur_dir + ran_file_name + '.c4z')
+            if os.path.isfile(cur_dir + ran_file_name + '.zip'):
+                os.remove(cur_dir + ran_file_name + '.zip')
             shutil.make_archive(ran_file_name, 'zip', temp_dir + '/driver')
-            base_path = os.path.splitext(cur_dir + '/' + ran_file_name + '.zip')[0]
-            os.rename(cur_dir + '/' + ran_file_name + '.zip', base_path + '.c4z')
+            base_path = os.path.splitext(cur_dir + ran_file_name + '.zip')[0]
+            os.rename(cur_dir + ran_file_name + '.zip', base_path + '.c4z')
         else:
-            if os.path.isfile(cur_dir + '/' + driver_name + '.c4z'):
-                os.remove(cur_dir + '/' + driver_name + '.c4z')
-            if os.path.isfile(cur_dir + '/' + driver_name + '.zip'):
-                os.remove(cur_dir + '/' + driver_name + '.zip')
+            if os.path.isfile(cur_dir + driver_name + '.c4z'):
+                os.remove(cur_dir + driver_name + '.c4z')
+            if os.path.isfile(cur_dir + driver_name + '.zip'):
+                os.remove(cur_dir + driver_name + '.zip')
             shutil.make_archive(driver_name, 'zip', temp_dir + '/driver')
-            base_path = os.path.splitext(cur_dir + '/' + driver_name + '.zip')[0]
-            os.rename(cur_dir + '/' + driver_name + '.zip', base_path + '.c4z')
+            base_path = os.path.splitext(cur_dir + driver_name + '.zip')[0]
+            os.rename(cur_dir + driver_name + '.zip', base_path + '.c4z')
 
         pop.destroy()
 
@@ -677,12 +622,12 @@ def export_c4z():
                 shutil.copy(orig_file_path, orig_file_path + '.' + temp_name)
                 os.remove(orig_file_path + '.' + temp_name)
             shutil.make_archive(temp_name, 'zip', temp_dir + '/driver')
-            base = os.path.splitext(cur_dir + '/' + temp_name + '.zip')[0]
-            os.rename(cur_dir + '/' + temp_name + '.zip', base + '.c4z')
+            base = os.path.splitext(cur_dir + temp_name + '.zip')[0]
+            os.rename(cur_dir + temp_name + '.zip', base + '.c4z')
             os.remove(orig_file_path)
             shutil.copy(base + '.c4z', orig_file_path)
             os.remove(base + '.c4z')
-        except IOError as x:
+        except IOError as _:
             pop = Toplevel(root)
             pop.title('Cannot Overwrite Original File')
             pop.geometry('239x95')
@@ -708,8 +653,8 @@ def export_c4z():
             yes_button = tk.Button(pop, text='Yes', width='10', command=confirm_write)
             yes_button.grid(row=2, column=0, sticky='w', padx=90)
     else:
-        if os.path.isfile(cur_dir + '/' + driver_name + '.c4z') or os.path.isfile(
-                cur_dir + '/' + driver_name + '.zip'):
+        if os.path.isfile(cur_dir + driver_name + '.c4z') or os.path.isfile(
+                cur_dir + driver_name + '.zip'):
             pop = Toplevel(root)
             pop.title('Overwrite')
             pop.geometry('239x70')
@@ -727,8 +672,8 @@ def export_c4z():
             yes_button.grid(row=2, column=1, sticky='w', padx=5)
         else:
             shutil.make_archive(driver_name, 'zip', temp_dir + '/driver')
-            base = os.path.splitext(cur_dir + '/' + driver_name + '.zip')[0]
-            os.rename(cur_dir + '/' + driver_name + '.zip', base + '.c4z')
+            base = os.path.splitext(cur_dir + driver_name + '.zip')[0]
+            os.rename(cur_dir + driver_name + '.zip', base + '.c4z')
 
 
 def restore_entry_text():
@@ -751,22 +696,17 @@ def restore_entry_text():
     root.after(2000, restore_entry_text)
 
 
-# Separators
+c4z_panel = C4zPanel()
+
 separator0 = ttk.Separator(root, orient='vertical')
 separator0.place(x=305, y=0, relwidth=0.2, relheight=1)
+
+replacement_panel = ReplacementPanel()
 
 separator1 = ttk.Separator(root, orient='vertical')
 separator1.place(x=610, y=0, relwidth=0.2, relheight=1)
 
-# Version Label
-version_label = Label(root, text=version).place(relx=1, rely=1.01, anchor='se')
-
-# Initialize
-c4z_panel = C4zPanel()
-replacement_panel = ReplacementPanel()
 export_panel = ExportPanel()
 
-root.after(0, restore_entry_text)
-root.mainloop()
-
-shutil.rmtree(temp_dir)
+# Version Label
+version_label = Label(root, text=version).place(relx=1, rely=1.01, anchor='se')
---
+++ create_exe.py
@@ -0,0 +1,31 @@
+import PyInstaller.__main__
+import shutil
+import os
+from C4IconSwapper import version
+from C4IconSwapper import cur_dir
+
+
+def clean_up():
+    if os.path.isfile(cur_dir + 'main.spec'):
+        os.remove(cur_dir + 'main.spec')
+    if os.path.isdir(cur_dir + 'dist/'):
+        shutil.rmtree(cur_dir + 'dist/')
+    if os.path.isdir(cur_dir + 'build/'):
+        shutil.rmtree(cur_dir + 'build/')
+    if os.path.isdir(cur_dir + '__pycache__'):
+        shutil.rmtree(cur_dir + '__pycache__')
+
+
+def move_rename():
+    if os.path.isfile(cur_dir + 'dist/main.exe'):
+        if not os.path.isfile(cur_dir + 'C4IconSwapper.' + str(version) + '.exe'):
+            shutil.copy(cur_dir + 'dist/main.exe', cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+        else:
+            os.remove(cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+            shutil.copy(cur_dir + 'dist/main.exe', cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+
+
+clean_up()
+PyInstaller.__main__.run(['main.py', '--onefile', '-w'])
+move_rename()
+clean_up()
