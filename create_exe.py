a3153d9f26c29e5d084962927af6af05e4ca910c
carlodelpizzo <carlodelpizzo@gmail.com>
2021-08-22 00:30:11 -0400

    Consolidated Code

--- create_exe.py
+++ create_exe.py
@@ -6,27 +6,43 @@ from C4IconSwapper import version
 cur_dir = os.getcwd() + '/'
 
 
-def clean_up():
-    if os.path.isfile(cur_dir + 'main.spec'):
-        os.remove(cur_dir + 'main.spec')
-    if os.path.isdir(cur_dir + 'dist/'):
-        shutil.rmtree(cur_dir + 'dist/')
-    if os.path.isdir(cur_dir + 'build/'):
-        shutil.rmtree(cur_dir + 'build/')
-    if os.path.isdir(cur_dir + '__pycache__/'):
-        shutil.rmtree(cur_dir + '__pycache__/')
+def make_exe(python_file: str):
+    if python_file[len(python_file) - 3: len(python_file)] != '.py':
+        return print('Not a python file...')
+    else:
+        python_file_name = python_file[0:len(python_file) - 3]
 
+    def clean_up():
+        if os.path.isfile(cur_dir + python_file_name + '.spec'):
+            os.remove(cur_dir + python_file_name + '.spec')
+        if os.path.isdir(cur_dir + 'dist/'):
+            shutil.rmtree(cur_dir + 'dist/')
+        if os.path.isdir(cur_dir + 'build/'):
+            shutil.rmtree(cur_dir + 'build/')
+        if os.path.isdir(cur_dir + '__pycache__/'):
+            shutil.rmtree(cur_dir + '__pycache__/')
 
-def move_rename():
-    if os.path.isfile(cur_dir + 'dist/main.exe'):
+    # Remove old builds if any
+    clean_up()
+    # Build Exe
+    PyInstaller.__main__.run([python_file, '--onefile', '-w'])
+    # Move and Rename
+    if os.path.isfile(cur_dir + 'dist/' + python_file_name + '.exe'):
         if not os.path.isfile(cur_dir + 'C4IconSwapper.' + str(version) + '.exe'):
-            shutil.copy(cur_dir + 'dist/main.exe', cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+            shutil.copy(cur_dir + 'dist/' + python_file_name + '.exe',
+                        cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
         else:
             os.remove(cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
-            shutil.copy(cur_dir + 'dist/main.exe', cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+            shutil.copy(cur_dir + 'dist/' + python_file_name + '.exe',
+                        cur_dir + 'C4IconSwapper.' + str(version) + '.exe')
+    # Remove build files
+    clean_up()
 
 
-clean_up()
-PyInstaller.__main__.run(['main.py', '--onefile', '-w'])
-move_rename()
-clean_up()
+overwrite_file = '_'
+if os.path.isfile(cur_dir + 'C4IconSwapper.' + str(version) + '.exe'):
+    overwrite_file = input('Overwrite file? (y/n)... ')
+    if overwrite_file[0] == 'y' or overwrite_file[0] == 'y':
+        make_exe('main.py')
+else:
+    make_exe('main.py')
