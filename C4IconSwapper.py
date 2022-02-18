cfd8b665e18b4e59bdcf9e58b294effd94289a2e
Bones Malin <carlodelpizzo@gmail.com>
2022-02-18 14:05:58 -0500

    test

--- C4IconSwapper.py
+++ C4IconSwapper.py
@@ -18,8 +18,6 @@ from XMLObject import XMLObject
 
 version = '5.2a'
 
-# Test
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 capital_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
