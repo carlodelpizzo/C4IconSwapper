b09ce38307fa9c23b65152710cd32f0cf4156a7d
Bones Malin <carlodelpizzo@gmail.com>
2022-07-04 18:07:16 -0400

    Updated for Mac

--- create_exe.py
+++ create_exe.py
@@ -49,31 +49,24 @@ def make_exe(python_file: str):
         PyInstaller.__main__.run([python_file, '--onefile', '-w', '--icon=icon.ico'])
         # Move and Rename
         if os.path.isfile(cur_dir + 'dist/' + python_file_name + '.exe'):
-            if not os.path.isfile(cur_dir + 'C4IconSwapper.' + version + '.exe'):
-                shutil.copy(cur_dir + 'dist/' + python_file_name + '.exe',
-                            cur_dir + 'C4IconSwapper.' + version + '.exe')
-            else:
+            if os.path.isfile(cur_dir + 'C4IconSwapper.' + version + '.exe'):
                 os.remove(cur_dir + 'C4IconSwapper.' + version + '.exe')
-                shutil.copy(cur_dir + 'dist/' + python_file_name + '.exe',
-                            cur_dir + 'C4IconSwapper.' + version + '.exe')
-    else:
+            shutil.copy(cur_dir + 'dist/' + python_file_name + '.exe',
+                        cur_dir + 'C4IconSwapper.' + version + '.exe')
+    elif on_mac:
         # Build Exe
         PyInstaller.__main__.run([python_file, '--onefile', '-w', '--icon=icon.icns'])
         # Move and Rename
-        if os.path.isfile(cur_dir + '/dist/' + python_file_name + '.app'):
-            if not os.path.isfile(cur_dir + '/C4IconSwapper.' + version + '.app'):
-                shutil.copy(cur_dir + '/dist/' + python_file_name + '.app',
-                            cur_dir + '/C4IconSwapper.' + version + '.app')
-            else:
-                os.remove(cur_dir + '/C4IconSwapper.' + version + '.app')
-                shutil.copy(cur_dir + '/dist/' + python_file_name + '.app',
-                            cur_dir + '/C4IconSwapper.' + version + '.app')
+        if os.path.isdir(cur_dir + '/C4IconSwapper.' + version + '.app'):
+            shutil.rmtree(cur_dir + '/C4IconSwapper.' + version + '.app')
+        shutil.copytree(cur_dir + '/dist/' + python_file_name + '.app',
+                        cur_dir + '/C4IconSwapper.' + version + '.app')
     # Remove build files
     clean_up()
 
 
 # Change to True before running; this is to prevent accidental run; add tkdnd2.8\ to Python\Python38\tcl\tcl8.6\
-execute = False
+execute = True
 
 if execute:
     if not on_mac:
@@ -84,9 +77,9 @@ if execute:
                 make_exe('main.py')
         else:
             make_exe('main.py')
-    else:
+    elif on_mac:
         overwrite_file = '_'
-        if os.path.isfile(cur_dir + '/C4IconSwapper.' + version + '.app'):
+        if os.path.isdir(cur_dir + '/C4IconSwapper.' + version + '.app'):
             overwrite_file = input('Overwrite file? (y/n)... ')
             if overwrite_file[0] == 'y' or overwrite_file[0] == 'Y':
                 make_exe('mainMac.py')
