4b6073f156f4f17773cbfbd8e3594651c9632006
Bones Malin <carlodelpizzo@gmail.com>
2022-09-17 17:48:14 -0400

    Fixed bug treating xml tag attribute as list instead of string

--- XMLObject.py
+++ XMLObject.py
@@ -42,7 +42,7 @@ def get_xml_data(xml_path=None, xml_string=None, tag_indexes=None):
     return output
 
 
-# xml_data = ['tag_name', 'tag_value', [tag_attributes], [children]]; Initialize with path to xml file
+# xml_data = ['tag_name', 'tag_value', 'tag_attributes', [children]]; Initialize with path to xml file
 class XMLObject:
     def __init__(self, xml_path=None, xml_data=None, parents=None):
         if xml_path:
