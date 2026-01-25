2563fb461a049f596e7fae9dd7327ac8da3c28ca
BonesMalin <carlodelpizzo@gmail.com>
2026-01-25 00:14:00 -0500

    Refactor

--- create_exe_nuitka.py
+++ create_exe_nuitka.py
@@ -5,37 +5,39 @@ import tkinterdnd2
 from C4IconSwapper import version
 
 main_file = 'C4IconSwapper.py'
-exe_file_name = 'C4IconSwapper'
+exe_file_name = f'C4IconSwapper.{version}'
 tkdnd_path = os.path.join(os.path.dirname(tkinterdnd2.__file__), 'tkdnd')
 
-command = [
-    sys.executable, '-m', 'nuitka',
-    '--standalone',
-    '--onefile',
-    # '--mingw64',
-    '--msvc=latest',
-    '--assume-yes-for-downloads',
-    '--enable-plugin=tk-inter',
-    '--windows-console-mode=disable',
-    '--windows-icon-from-ico=assets/icon.ico',
-    f'--output-filename={exe_file_name}.{version}.exe',
-    '--include-data-dir=assets=assets',
-    # '--include-package=tkinterdnd2',
-    # '--collect-all=tkinterdnd2',
-    f'--include-data-dir={tkdnd_path}=tkinterdnd2/tkdnd',
-    main_file
-]
+
+def make_exe():
+    nuitka_command = [
+        sys.executable, '-m', 'nuitka',
+        '--standalone',
+        '--onefile',
+        '--msvc=latest',
+        '--assume-yes-for-downloads',
+        '--enable-plugin=tk-inter',
+        '--windows-console-mode=disable',
+        '--windows-icon-from-ico=assets/icon.ico',
+        f'--output-filename={exe_file_name}.exe',
+        '--include-data-dir=assets=assets',
+        f'--include-data-dir={tkdnd_path}=tkinterdnd2/tkdnd',
+        main_file
+    ]
+
+    result = subprocess.run(nuitka_command)
+
+    if result.returncode == 0:
+        print('--- Build Completed ---')
+    else:
+        print('--- Build Failed or Interrupted ---')
+
 
 # Change to True before running; this is to prevent accidental run
 execute = False
 
 if __name__ == '__main__':
     if execute:
-        result = subprocess.run(command)
-
-        if result.returncode == 0:
-            print('--- Build Completed ---')
-        else:
-            print('--- Build Failed or Interrupted ---')
+        make_exe()
     else:
         print(f'{os.path.basename(__file__)} execution set to False')
