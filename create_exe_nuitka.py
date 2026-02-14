56d1b14bcc36d3a71ea288a2ca9724a90a7cbb05
BonesMalin <carlodelpizzo@gmail.com>
2026-02-13 23:04:29 -0500

    Changed comment

--- create_exe_nuitka.py
+++ create_exe_nuitka.py
@@ -34,7 +34,7 @@ def make_exe():
 
 
 # pip install nuitka zstandard ordered-set
-# The program was made for Python 3.11, not sure if other version will work
+# App made using Python 3.11, not sure if other versions will work
 # Change to True before running; this is to prevent accidental run
 execute = False
 
